2e78dc02f81c629ac7626d125a5fbd604a447afe
Sandali dilrukshi <dsandali290@gmail.com>
2025-08-10 12:47:08 +0530

    Initial commit

---
+++ src/main/java/com/store_management/store_management/HelloController.java
@@ -0,0 +1,14 @@
+package com.store_management.store_management;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.store_management.store_management {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.store_management.store_management to javafx.fxml;
+    exports com.store_management.store_management;
+}
